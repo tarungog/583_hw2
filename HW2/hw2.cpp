702952ce99dcc6e1263e711a1079661791c947d9
Tarun Gogineni <tgog@umich.edu>
2019-10-18 13:51:41 -0400

    a

--- HW2/hw2.cpp
+++ HW2/hw2.cpp
@@ -436,18 +436,17 @@ bool Correctness::LoopInvariantCodeMotion::runOnLoop(
 
                       auto new_instruction = I2.clone();
 
-                      errs() << "before changes" << "\n";
+                      // errs() << "before changes" << "\n";
 
-                      print_basic_block(block2);
+                      // print_basic_block(block2);
 
                       new_instruction->insertAfter(&I2);
                       errs() << "inserted load: " << *new_instruction << "\n";
                       errs() << "op0 " << new_instruction->getOperand(0) << " op1 ";
                       errs() << new_instruction->getOperand(1) << "\n";
-                      errs() << "after changes" << "\n";
+                      // errs() << "after changes" << "\n";
 
-                      print_basic_block(block2);
-
+                      // print_basic_block(block2);
 
                       Changed = true;
                     }
@@ -456,7 +455,6 @@ bool Correctness::LoopInvariantCodeMotion::runOnLoop(
               }
             }
           }
-
         }
       }
     }
